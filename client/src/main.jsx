fbdefd4d22a422c537c1f5a1ded0c128c8b082c5
Krish8321 <krishchoudhari10@gmail.com>
2026-04-23 21:07:13 +0530

    client side cors changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,9 @@ import './index.css'
 import App from './App.jsx'
 import axios from 'axios';
 
+// axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+axios.defaults.baseURL = '/api';
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
